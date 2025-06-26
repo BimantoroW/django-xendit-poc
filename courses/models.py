42c2adec59d7957526d098f337dd033cb27da30c
BimantoroW <bimantoro.widyadana@ui.ac.id>
2025-06-26 14:21:00 +0700

    feat(courses): define models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.IntegerField()
+
+class Enrollment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(auto_now_add=True)
