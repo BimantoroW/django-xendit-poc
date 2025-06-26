8810ff9cacb592bf9c1d5d118445b2d829d7753a
BimantoroW <bimantoro.widyadana@ui.ac.id>
2025-06-26 14:22:32 +0700

    feat(orders): define models

--- course_checkout_poc/settings.py
+++ course_checkout_poc/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    'courses.apps.CoursesConfig'
+    'courses.apps.CoursesConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from courses.models import Course
 
-# Create your models here.
+class Order(models.Model):
+    class OrderStatus(models.TextChoices):
+        PENDING = 'PENDING'
+        PAID = 'PAID'
+        EXPIRED = 'EXPIRED'
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status = models.CharField(max_length=16, choices=OrderStatus, default=OrderStatus.PENDING)
+    total_amount = models.IntegerField()
+
+    xendit_invoice_id = models.CharField(max_length=255)
+    xendit_invoice_url = models.URLField(max_length=1024)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
