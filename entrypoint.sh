66da74d8f52ad062183c693849a639f1c40bb902
BimantoroW <bimantoro.widyadana@ui.ac.id>
2025-06-28 22:58:00 +0700

    chore(deployment): use gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python3 manage.py flush --no-input \
-    && python3 manage.py migrate \
+python3 manage.py migrate \
     && python3 manage.py seed \
-    && python3 manage.py runserver 0.0.0.0:8000
+
+gunicorn course_checkout_poc.wsgi:application --bind 0.0.0.0:8000
